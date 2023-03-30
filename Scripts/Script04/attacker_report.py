3e6c22b47415980727be925caaa2e6d9aaecebab
Alex <89423205+MineMyThoughts@users.noreply.github.com>
2023-03-30 00:38:59 -0400

    Create attacker_report.py

---
+++ Scripts/Script04/attacker_report.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+# Alex Tedesco
+# 3/28/2023
+
+# Imports:
+import os
+from collections import Counter
+from geoip import geolite2
+import datetime
+
+# Clears the terminal upon running the script
+os.system ("clear")
+
+with open('syslog.log') as file:
+    ipaddresses = []
+
+    for line in file:
+        line = line.strip()
+        line = line.split(": ")
+        # print (line[1])
+        
+        if line[1].startswith("Failed password for invalid user"):
+            # print (line[1])
+            failedpasswd = line[1]
+            failedpasswd = failedpasswd.split(" ")
+            # print (failedpasswd[7])
+            ipaddresses.append(failedpasswd[7])
+        
+        elif line[1].startswith("Failed password for root"):
+            # print (line[1])
+            failedpasswd = line[1]
+            failedpasswd = failedpasswd.split(" ")
+            # print (failedpasswd[5])
+            ipaddresses.append(failedpasswd[5])
+
+    counts = Counter(ipaddresses)
+
+counts = counts.most_common()
+
+date = datetime.datetime.now()
+
+print ("\u001b[32m" + "Attacker Report" + "\u001b[37m" + " - " + date.strftime ("%B") + " " + date.strftime ("%d") + ", " + date.strftime ("%Y") + "\n")
+print ('\u001b[31m' + "COUNT           IP Address              COUNTRY" + "\u001b[37m")
+
+for i in counts[::-1]:
+    if i[1] >= 20:
+        ip_address = str(i[0])
+        count = str(i[1])
+        country = geolite2.lookup(i[0]).country
+        print (count  + "             " + ip_address + "        " + country)
